f4ebdf60fd5412be66bf8870f174557fcd418d59
刘冬涵 <liudonghan@lawxp.com>
2022-12-30 10:30:33 +0800

    修复部分代码

--- app/src/main/java/com/example/mavenplugin/MainActivity.java
+++ app/src/main/java/com/example/mavenplugin/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.jitpack.JitPackUtils;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
