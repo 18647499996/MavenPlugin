ff74e6a085e5027a74c53689714b4e886b68df2c
刘冬涵 <liudonghan@lawxp.com>
2022-12-30 10:39:50 +0800

    添加代码

---
+++ JitPack/src/main/java/com/example/jitpack/JitPackUtils.java
@@ -0,0 +1,35 @@
+package com.example.jitpack;
+
+import android.util.Log;
+
+/**
+ * Description：
+ *
+ * @author Created by: Li_Min
+ * Time:12/30/22
+ */
+public class JitPackUtils {
+    private static volatile JitPackUtils instance = null;
+
+    private JitPackUtils(){}
+
+    public static JitPackUtils getInstance(){
+     //single chcekout
+     if(null == instance){
+        synchronized (JitPackUtils.class){
+            // double checkout
+            if(null == instance){
+                instance = new JitPackUtils();
+            }
+        }
+     }
+     return instance;
+    }
+
+    /**
+     * 初始化
+     */
+    private void initJitPack(){
+        Log.d("tag","第一个插件项目");
+    }
+}
