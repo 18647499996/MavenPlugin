f81c9f218a39990af943252d00de11c55b129db8
刘冬涵 <liudonghan@lawxp.com>
2022-12-30 09:42:31 +0800

    修复代码

--- JitPack/build/generated/source/buildConfig/release/com/example/jitpack/BuildConfig.java
+++ JitPack/build/generated/source/buildConfig/release/com/example/jitpack/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.example.jitpack";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final String VERSION_NAME = "null";
 }
